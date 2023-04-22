687c2dc401a87130cbf265506a02a5dd362eb487
takeshi-1000 <stubgurssy@gmail.com>
2023-04-23 08:03:21 +0900

    ColorExtensionテストコード一部追加

--- Sources/Utility/NSColor+Extension.swift
+++ Sources/Utility/NSColor+Extension.swift
@@ -1,7 +1,7 @@
 import Cocoa
 
 public extension NSColor {
-    convenience init(hex: String, alpha: CGFloat = 1.0) {
+    convenience init(hex: String, alpha: Double = 1.0) {
         let v = Int("000000" + hex, radix: 16) ?? 0
         let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
         let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
--- Tests/UtilityTests/NSColorExtensionTests.swift
+++ Tests/UtilityTests/NSColorExtensionTests.swift
@@ -2,10 +2,26 @@ import XCTest
 @testable import Utility
 
 final class NSColorExtensionTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual("Hello, World!", "Hello, World!")
+    func testConvenienceInit() {
+        XCTContext.runActivity(named: "alphaの値確認") { _ in
+            XCTContext.runActivity(named: "alpha指定あり") { _ in
+                let alpha: Double = 0.45
+                let hex = "FFFFFF"
+                let actual = NSColor(hex: hex, alpha: alpha)
+                
+                XCTAssertEqual(actual.alphaComponent, alpha)
+            }
+            XCTContext.runActivity(named: "alpha指定なし") { _ in
+                let hex = "FFFFFF"
+                let actual = NSColor(hex: hex)
+                let expectedAlpha: Double = 1.0
+                
+                XCTAssertEqual(actual.alphaComponent, expectedAlpha)
+            }
+        }
+        
+        XCTContext.runActivity(named: "hexが正しくrgbに変換できているかの確認") { _ in
+            
+        }
     }
 }
