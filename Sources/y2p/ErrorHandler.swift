2683c007b92e00bcca364fb74708786ac1264002
takeshi-1000 <stubgurssy@gmail.com>
2023-04-25 09:26:19 +0900

    細かな修正

--- Sources/y2p/ErrorHandler.swift
+++ Sources/y2p/ErrorHandler.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class ErrorHandler {
+enum ErrorHandler {
     static func handle(_ error: Error) -> String? {
         let errorStr: (_ cause: String, _ detail: String) -> String = { cause, detail in
             return "原因: \(cause)\n詳細: \(detail)"
