801cefb6a86209f3616e647e2e30175f6fe0ff81
takeshi-1000 <stubgurssy@gmail.com>
2023-04-27 08:36:47 +0900

    svgで出力した際にurlを付与するための準備

--- Tests/DataTests/ViewTests.swift
+++ Tests/DataTests/ViewTests.swift
@@ -3,6 +3,7 @@ import XCTest
 
 final class ViewTests: XCTestCase {
     let mockViewData: View = .init(nameData: (key: "ListView", value: "リスト"),
+                                   urlStr: "https://www.youtube.com",
                                    transitionTypeKey: "modal",
                                    contentColor: "123456",
                                    borderColor: "654321",
@@ -12,6 +13,7 @@ final class ViewTests: XCTestCase {
     func test_init() {
         XCTContext.runActivity(named: "初期化時に適切に値をセットできているかの確認") { _ in
             let nameData: (key: String, value: String) = ("SplashView", "スプラッシュ")
+            let urlStr = "https://www.youtube.com"
             let transitionTypeKey: String = "push"
             let contentColor: String = "FFFFFF"
             let borderColor: String = "000000"
@@ -19,6 +21,7 @@ final class ViewTests: XCTestCase {
             let views: [View] = [mockViewData, mockViewData, mockViewData]
             
             let actual = View(nameData: nameData,
+                              urlStr: urlStr,
                               transitionTypeKey: transitionTypeKey,
                               contentColor: contentColor,
                               borderColor: borderColor,
@@ -27,6 +30,7 @@ final class ViewTests: XCTestCase {
             
             XCTAssertEqual(actual.nameData.key, nameData.key)
             XCTAssertEqual(actual.nameData.value, nameData.value)
+            XCTAssertEqual(actual.urlStr, urlStr)
             XCTAssertEqual(actual.transitionTypeKey, transitionTypeKey)
             XCTAssertEqual(actual.contentColor, contentColor)
             XCTAssertEqual(actual.borderColor, borderColor)
